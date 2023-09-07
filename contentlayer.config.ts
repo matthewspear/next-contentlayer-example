d5d291ebe9a92da2895439391da86676d931df48
Matt Spear <matthewtimothyspear@gmail.com>
2023-09-07 13:40:52 +0700

    Add computed properties

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,9 +1,30 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import {
+  ComputedFields,
+  defineDocumentType,
+  makeSource,
+} from "contentlayer/source-files";
+import readingTime from "reading-time";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
 import rehypeCodeTitles from "rehype-code-titles";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypePrism from "rehype-prism-plus";
+
+const computedFields: ComputedFields = {
+  readingTime: { type: "json", resolve: (doc) => readingTime(doc.body.raw) },
+  wordCount: {
+    type: "number",
+    resolve: (doc) => doc.body.raw.split(/\s+/gu).length,
+  },
+  slug: {
+    type: "string",
+    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ""),
+  },
+  url: {
+    type: "string",
+    resolve: (doc) => `/posts/${doc._raw.flattenedPath}`,
+  },
+};
 
 const Post = defineDocumentType(() => ({
   name: "Post",
@@ -21,12 +42,7 @@ const Post = defineDocumentType(() => ({
       required: true,
     },
   },
-  computedFields: {
-    url: {
-      type: "string",
-      resolve: (doc) => `/posts/${doc._raw.flattenedPath}`,
-    },
-  },
+  computedFields: computedFields,
 }));
 
 export default makeSource({
